d50715efe978a6d363747a6cdc9e83a334fe64ba
RaphaelBonobo <raphael.bonn@gmail.com>
2021-01-23 00:07:25 +0100

    commit on 'main'

---
+++ src/res/color.js
@@ -0,0 +1,13 @@
+// The main theming colors as defined by this file
+
+const Colors = {
+  primary: '#1292B4',
+  secondary: '#FFF',
+  white: '#FFF',
+  lighter: '#F3F3F3',
+  light: '#DAE1E7',
+  dark: '#444',
+  black: '#000',
+};
+
+export default Colors;
---
+++ src/res/font.js
@@ -0,0 +1,9 @@
+// Meant to hold all custom styling added to our font and the main font used by our app
+// Main variables will be size, weight, family and so on
+
+const Fonts = {
+  headerSize: 40,
+  primaryTypo: 'sans-serif-condensed',
+};
+
+export default Fonts;
